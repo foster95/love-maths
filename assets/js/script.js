17bba1f82c61edb56e16c80c87f6cd2d92188a1d
foster95 <alicerosefoster95@gmail.com>
2025-06-18 13:14:15 +0100

    add increment score and increment wrong answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore()
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer ();
     }
 
     runGame(calculatedAnswer[1]);
@@ -92,9 +94,17 @@ function incrementScore() {
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and
+ * increments it by 1
+ */
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
+
 
 function displayAdditionQuestion(operand1, operand2) {
 
